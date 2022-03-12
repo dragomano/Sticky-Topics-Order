1d457855ecb7010343721ea18fd8f1d57841ec83
Bugo <bugo@dragomano.ru>
2022-03-12 14:45:01 +0500

    Remove legacy_smf20 dir

--- legacy_smf20/hook_StickyTopicsOrder.english.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$txt['StickyTopicsOrder_order'] = 'Order (Weight)';
-$txt['StickyTopicsOrder_order_new'] = 'New order (New weight)';
-$txt['StickyTopicsOrder_change'] = 'set order';
-
--- legacy_smf20/hook_StickyTopicsOrder.russian.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$txt['StickyTopicsOrder_order'] = '"Важность" темы';
-$txt['StickyTopicsOrder_order_new'] = 'Установить "важность" темы';
-$txt['StickyTopicsOrder_change'] = 'Изменить порядок';
-
